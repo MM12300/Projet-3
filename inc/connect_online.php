34435a8288e2afa22dd852b7113664599622e1e3
MM12300 <nerggalkore@gmail.com>
2020-06-04 09:53:17 +0200

    ?

--- inc/connect_online.php
+++ inc/connect_online.php
@@ -1,8 +1,17 @@
 <?php
-require_once('inc/brouette.php');
+//*********************** */
+//Fichier pour se connecter à une BDD (ici bdd_blog)
+//*********************** */
+
+//$db === Base de Données (BDD)
+//hôte : local
+//nom de la base de données : bdd (exemple)
+//login : root - mot de passe : '' (vide)
+
+
 try {
     //Connexion à la base de données
-    $db = new PDO('mysql:host='. $Aa . ';dbname=' . $Bb . ',' . $Cc . ',' . $Dd);
+    $db = new PDO('mysql:host=wisconsin.o2switch.net;dbname=mama5881_cv', 'mama5881', 'KYfF-3vCV2ky');
 
     //On force les échanges en UTF8
     $db->exec('SET NAMES "UTF8"');
