7bda337d3180eb975c96f09273418095b5fe2d5f
MM12300 <nerggalkore@gmail.com>
2020-08-08 14:48:42 +0200

    secu

--- inc/close.php
+++
@@ -1,8 +0,0 @@
-<?php
-//*********************** */
-//Fichier pour se terminer la connexion à une BDD 
-//*********************** */
-
-//$db corresponds à la variable BDD qui est définie dans connect.php
-
-$db=null;
